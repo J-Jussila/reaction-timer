1ae8bedd655d47c583860421db5c1af740f1fcd0
J-Jussila <jussilajani@protonmail.com>
2025-07-21 10:13:28 +0300

    Simple reaction timer

--- reaction_timer.py
+++ reaction_timer.py
@@ -1,1 +1,21 @@
-print("reaction timer!")
+import time
+import random
+
+print("Reaction Timer Game")
+print("When you see 'GO!', press Enter as quickly as you can.")
+input("Press Enter to start...")
+
+# Odota random aika
+wait_time = random.uniform(3, 7)
+print("Get ready...")
+time.sleep(wait_time)
+
+print("Go!")
+start_time = time.time()
+
+input() # Odota että painetaan Enter
+
+end_time = time.time()
+reaction_time = end_time - start_time
+
+print(f"Your reaction time is {reaction_time:.3f} seconds.")
